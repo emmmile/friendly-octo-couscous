8af38fff9554e1705a7573a1b6e6bfd82cc0a2c1
Emilio Del Tessandoro <emilio.deltessa@gmail.com>
2016-09-04 22:17:15 +0200

    some more stuff

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 To run the program you do:
 
-```
+```bash
 # eventually set a virtualenv :)
 pip install -r requirements.txt
 
-# download twitter.com and all the links under the element with class Grid-cell
+# download twitter.com and all the links under the element with class nav
 # if changed every 120 seconds
-python3 fetch.py http://www.twitter.com 120
+python3 fetch.py http://www.twitter.com 120 nav
 ```
--- fetch.py
+++ fetch.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python3
 
 import collections
-import hashlib
 import httpcache
 import logging
 import lxml.html
@@ -10,9 +9,10 @@ import requests
 import retrying
 import sys
 import time
+import urllib
 
-
-@retrying.retry(stop_max_attempt_number=8, wait_exponential_multiplier=1000, wait_exponential_max=64000)
+# some utility functions
+@retrying.retry(stop_max_attempt_number=4, wait_exponential_multiplier=1000, wait_exponential_max=32000)
 def get_page_by_uri(session, uri):
     headers = {
         # otherwise nginx might reply with 403 Forbidden
@@ -36,15 +36,17 @@ def save_page_in(path, filename, content):
 # Every function takes a state and returns a new function to execute on the
 # modified state. The machine is then executed by the Machine class.
 class State:
-    BASE_URI = sys.argv[1]
-    SLEEP = int(sys.argv[2])
+    uri = sys.argv[1]
+    sleep = int(sys.argv[2])
+    element = sys.argv[3]
 
     session = None
     response = None
     previous = None
     hashes = collections.deque(maxlen=1024*1024)
 
-
+# The current function returns the next function to executed.
+# It's quite nice that if you don't return anything the machine will stop.
 class Machine:
     def __init__(self, state, f):
         while f:
@@ -53,9 +55,9 @@ class Machine:
 
 def create_session(state):
     s = requests.Session()
+    state.session = s
     s.mount('http://', httpcache.CachingHTTPAdapter())
     s.mount('https://', httpcache.CachingHTTPAdapter())
-    state.session = s
     logging.info("Created HTTP session with cache")
     return fetch_index
 
@@ -63,18 +65,19 @@ def create_session(state):
 # get a page using the HTTP cache, if the page is not modified return the one
 # that is in the internal cache
 def fetch_index(state):
-    logging.info("Fetching page %s", state.BASE_URI)
-    state.response = get_page_by_uri(state.session, state.BASE_URI)
+    logging.info("Fetching page %s", state.uri)
+    state.response = get_page_by_uri(state.session, state.uri)
     return check_source
 
 
 # check if the content of the page is changed or not
 def check_source(state):
     if not state.previous or state.response.text != state.previous.text:
-        current_hash = hashlib.md5(state.response.text.encode('utf-8')).hexdigest()
+        current_hash = hash(state.response.text)
         if current_hash in state.hashes:
             logging.warning("Page changed but content is not new")
 
+        logging.info(state.hashes)
         state.hashes.append(current_hash)
         state.previous = state.response
         logging.info("Page changed")
@@ -91,15 +94,19 @@ def save_content(state):
     save_page_in(path, "main.html", state.response.text)
 
     dom = lxml.html.fromstring(state.response.text)
-    for link in dom.xpath(xpath_for_class('subpage-content') + '//a/@href'):
+    for index, link in enumerate(dom.xpath(xpath_for_class(state.element) + '//a/@href')):
+        if 'http://' not in link:
+            link = urllib.parse.urljoin(state.uri, link)
+
+        # TODO if the link is broken???
         link_response = get_page_by_uri(state.session, link)
-        save_page_in(path, "child.html", link_response.text)
+        save_page_in(path, "child-" + str(index) + ".html", link_response.text)
 
     return sleep
 
 
 def sleep(state):
-    time.sleep(state.SLEEP)
+    time.sleep(state.sleep)
     return fetch_index
 
 
